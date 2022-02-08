b49a783903558d22edf185ab7c5a1d04cf332e5c
Sravan <saisraone2002@gmail.com>
2022-02-08 18:23:10 +0530

    PLP:38 : getAllCompanies Query
    after adding prettier extension

--- server/src/graphql/resolvers/company/queries.graphql.ts
+++ server/src/graphql/resolvers/company/queries.graphql.ts
@@ -1,5 +1,6 @@
 import { ApolloContext } from "../../../context";
 import { Company,QueryResolvers } from "../../../types/graphql";
+
 export const queries: QueryResolvers<ApolloContext, Company> = {
 	getAllCompanies: async (_, {}, { prisma }:ApolloContext) => {
 		const companies: Company[] | null = await prisma.company.findMany({
