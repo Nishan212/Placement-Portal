bdf5fe71b094a8da2e5b88f824c19006a12ae1d6
Sravan <saisraone2002@gmail.com>
2022-02-08 18:18:04 +0530

    PLP:38 : getAllCompanies Query
    Added context as ApolloContext

--- server/src/graphql/resolvers/company/queries.graphql.ts
+++ server/src/graphql/resolvers/company/queries.graphql.ts
@@ -1,8 +1,7 @@
 import { ApolloContext } from "../../../context";
 import { Company,QueryResolvers } from "../../../types/graphql";
-
 export const queries: QueryResolvers<ApolloContext, Company> = {
-	getAllCompanies: async (_, {}, { prisma }) => {
+	getAllCompanies: async (_, {}, { prisma }:ApolloContext) => {
 		const companies: Company[] | null = await prisma.company.findMany({
 				
 		});
